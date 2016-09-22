27b1f5f5aa877eca1f9cfd8361ff46de24087ba9
Kaleb Klein <klein.jae@gmail.com>
2016-09-21 20:53:19 -0400

    Oops, forgot some doc blocks in C# code ;)
    Signed-off-by: Kaleb Klein <klein.jae@gmail.com>

--- csharp/barista/Barista.cs
+++ csharp/barista/Barista.cs
@@ -19,12 +19,24 @@ namespace barista
 	/// </summary>
 	public class Barista
 	{
+		/// <summary>
+		/// First String
+		/// </summary>
 		private string str1;
 
+		/// <summary>
+		/// Second String
+		/// </summary>
 		private string str2;
 
+		/// <summary>
+		/// Third String
+		/// </summary>
 		private string str3;
 
+		/// <summary>
+		/// Class Constructor
+		/// </summary>
 		public Barista()
 		{
 			this.str1 = "ers";
@@ -32,12 +44,22 @@ namespace barista
 			this.str3 = "amet";
 		}
 
+		/// <summary>
+		/// Process the request
+		/// </summary>
+		/// <param name="preference">The user's prefered choice of coffee</param>
+		/// <returns>The request with the secret code</returns>
 		public string request(string preference)
 		{
 			return preference + ". Secret code: "
 				+ str2 + str3 + str1;
 		}
 
+		/// <summary>
+		/// Reverse a given string
+		/// </summary>
+		/// <param name="s">The string to reverse</param>
+		/// <returns>The reversed string</returns>
 		private string reverse(string s)
 		{
 			char[] charA = s.ToCharArray();
